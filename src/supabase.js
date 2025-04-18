029e33f99523562564750efabc7862556238caf6
Rubik <56951629+TauraYouhei@users.noreply.github.com>
2025-04-18 12:50:54 +0900

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://vkmpvtwcwhsdgpslcbhm.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrbXB2dHdjd2hzZGdwc2xjYmhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc5OTY4MzgsImV4cCI6MjAwMzU3MjgzOH0.K7ZfzDljxDhQPQ3TCH6fjA6gkRJ33BhaG7KyjWSyXe8"
+  "https://kpezezvyzkzshtgbhxdq.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwZXplenZ5emt6c2h0Z2JoeGRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5NDY3NTAsImV4cCI6MjA2MDUyMjc1MH0.Z4HIuXtexBW0aaZj-f5__RYfZkGLGnMNmTmz1arDa-o"
 );
